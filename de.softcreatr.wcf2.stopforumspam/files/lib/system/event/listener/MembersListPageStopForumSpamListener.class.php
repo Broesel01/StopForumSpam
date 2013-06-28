27eec5073c67f394a3dbd68f7b5c652710e9efd4
Sascha Greuel <sascha@softcreatr.de>
2013-06-29 00:42:00 +0200

    Beta 1

--- de.softcreatr.wcf2.stopforumspam/files/lib/system/event/listener/MembersListPageStopForumSpamListener.class.php
+++ de.softcreatr.wcf2.stopforumspam/files/lib/system/event/listener/MembersListPageStopForumSpamListener.class.php
@@ -2,13 +2,12 @@
 namespace wcf\system\event\listener;
 use wcf\data\user\User;
 use wcf\system\event\IEventListener;
-use wcf\util\StopForumSpam;
 
 /**
  * Hide spammers from members list (if enabled)
  * 
  * @author	Sascha Greuel <sascha@softcreatr.de>
- * @copyright	2013 Sascha Greuel
+ * @copyright	2010-2013 Sascha Greuel
  * @license	Creative Commons BY-SA <http://creativecommons.org/licenses/by-sa/3.0/>
  * @package	de.softcreatr.wcf2.stopforumspam
  * @subpackage	system.event.listener
@@ -20,12 +19,12 @@ class MembersListPageStopForumSpamListener implements IEventListener {
 	 */
 	public function execute($eventObj, $className, $eventName) {
 		// Do nothing, if the module is disabled
-		if (!defined('MODULE_STOPFORUMSPAM') || !MODULE_STOPFORUMSPAM) {
+		if (!MODULE_STOPFORUMSPAM) {
 			return false;
 		}
 
 		// Hide all users, which are marked as spammer
-		if (defined('STOPFORUMSPAM_HIDEFROMML') && STOPFORUMSPAM_HIDEFROMML) {
+		if (STOPFORUMSPAM_HIDEFROMML) {
 			$eventObj->objectList->getConditionBuilder()->add('(SELECT userOption' . User::getUserOptionID('stopforumspam_userstatus') . '
 					FROM wcf' . WCF_N . '_user_option_value
 					WHERE userID = user_table.userID) != ?', array(2)
